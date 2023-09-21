80cdd4b9d107ba13ff3a90af36f454b645901971
DESKTOP-E1MV4TV\daw1 <ajimenezp59@educarex.es>
2023-09-21 17:10:28 +0200

    Proyecto Final

--- Proyecto/src/App.java
+++ Proyecto/src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hola a todos!");
+        System.out.println("Hola a todos/as!");
     }
 }
